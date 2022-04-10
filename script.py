6dc5213daf9f9d7150828fff9bff3097e1a306d0
Daniel Frederick <dannyfrederick360@gmail.com>
2022-04-10 13:29:39 -0400

    hurray

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ from azure.ai.textanalytics import (
     RecognizeEntitiesAction,
     AnalyzeSentimentAction,
 )
+import datetime
 
 class Course:
     def __init__(self, id, course_code, name):
@@ -25,9 +26,10 @@ class Assignment:
             self.key_phrases = self.extract_key_phrases()
         else:
             self.key_phrases = ['not gathered, srry']
+        self.score = self.calculate_score()
 
     def extract_key_phrases(self):
-        credential = AzureKeyCredential('TEXT_ANYL_KEY')
+        credential = AzureKeyCredential('447b1fc8efcf4e998cae155f64976c5c')
         endpoint = os.environ['TEXT_ANYL_ENDPOINT']
 
         text_analytics_client = TextAnalyticsClient(endpoint, credential)
@@ -42,6 +44,26 @@ class Assignment:
         result = [doc for doc in response if not doc.is_error]
 
         return result[0].key_phrases
+
+    def calculate_score(self):
+        score = 100
+
+        score_phrases = {
+            "final": 100,
+            "end of term": 100,
+            "essay": 75,
+            "reference page": 50,
+            "required reference page": 50,
+            "quiz": 50,
+            "homework": 25,
+            "MS Word": 10
+        }
+
+        for phrase in self.key_phrases:
+            if phrase in score_phrases:
+                score += score_phrases[phrase]
+
+        return score
 
 class Prioritizer:
     def __init__(self):
@@ -80,26 +102,30 @@ class Prioritizer:
 
                 for assignment in assignments_dict:
                     # print("\n{} - {}\nDescription: {}\nDue at: {}\nPoints Possible: {}\nCourse: {}".format(assignment['id'], assignment['name'], assignment['description'], assignment['due_at'], assignment['points_possible'], assignment['course_id']))
-                    if len(assignments) < 10:
-                       try:
-                           new_assignment_obj = Assignment(assignment['id'], assignment['name'], assignment['description'], assignment['due_at'], assignment['points_possible'], assignment['course_id'], course)
-                           assignments.append(new_assignment_obj)
-                           # print("\n\tkeywords for assignment: {} - \n{}\n".format(new_assignment.name, new_assignment.key_phrases))
-                       except Exception as e:
-                           print("exception occurred gathering assignment info for assignment: {} - \n{}\n".format(assignment['id'], e))
-                    else:
-                       print("10 assignments reached -- skipping azure api call")
-                       new_assignment_obj = Assignment(assignment['id'], assignment['name'], assignment['description'], assignment['due_at'], assignments['allowed_extensions'], assignment['points_possible'], assignment['course_id'], current_course, False)
-                       assignments.append(new_assignment_obj)
+                    try:
+                        new_assignment_obj = Assignment(assignment['id'], assignment['name'], assignment['description'], assignment['due_at'], assignment['points_possible'], assignment['course_id'], course)
+                        assignments.append(new_assignment_obj)
+                        # print("\n\tkeywords for assignment: {} - \n{}\n".format(new_assignment.name, new_assignment.key_phrases))
+                    except Exception as e:
+                        print("exception occurred gathering assignment info for assignment: {} - \n{}\n".format(assignment['id'], e))
             except Exception as e:
                 print("exception occurred gathering assignments for course: {} - \n{}".format(course.id, e))
         return assignments
 
-    def print_prioritized_assignments(self):
+    def prioritize(assignments):
+        pass
+
+    def print_assignments(self):
         for i in range(len(self.assignments) - 1):
             a = self.assignments[i]
-            print("\nPos: {}\n{} - {}\nDue at: {}\nPoints Possible: {}\nCourse: {}".format(i, a.id, a.name, a.due_at, a.points_possible, a.course_id))
+            print("\nPos: {}\n{} - {}\nDue at: {}\nPoints Possible: {}\nCourse: {}\nKey Phrases: {}\nScore: {}".format(i, a.id, a.name, a.due_at, a.points_possible, a.course_id, a.key_phrases, a.score))
 
+    def print_prioritized_assignments(self):
+        #prioritized_assignments = sorted(self.assignments, key=operator.attrgetter("score"))
+        prioritized_assignments = sorted(self.assignments, key=lambda assignment:assignment.score, reverse=True)
+        for i in range(len(prioritized_assignments) - 1):
+            a = prioritized_assignments[i]
+            print("\nPos: {}\n{} - {}\nDue at: {}\nPoints Possible: {}\nCourse: {}\nKey Phrases: {}\nScore: {}".format(i, a.id, a.name, a.due_at, a.points_possible, a.course_id, a.key_phrases, a.score))
 p = Prioritizer()
 p.print_prioritized_assignments()
 # print(p.assignments)
